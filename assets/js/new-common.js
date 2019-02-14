4de8e55333122424e37c5583e85543cdb4a421ff
안홍선 <37172347+cd-hsahn@users.noreply.github.com>
2019-02-14 14:21:21 +0900

    Update guide.html (#9)

--- assets/js/new-common.js
+++ assets/js/new-common.js
@@ -13,9 +13,23 @@ var inputClear = function() {
     });
 };
 
+var setDatePicker = function() {
+    $.datepicker.setDefaults({
+        dateFormat: 'yy-mm-dd',
+        dayNames: ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
+        dayNamesMin: ['월', '화', '수', '목', '금', '토', '일'],
+        monthNames: ['1월', '2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+        monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+    });
+    $('.input-date').datepicker();
+};
+
 var init = function() {
     if ($('.input-clear').length) {
         inputClear();
+    }
+    if ($('.input-date').length) {
+        setDatePicker();
     }
 };
 
