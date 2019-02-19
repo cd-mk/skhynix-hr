4852427682081a3a8a35e63de40e9ee491e77cc8
안홍선 <37172347+cd-hsahn@users.noreply.github.com>
2019-02-19 17:49:19 +0900

    커리어패스 : 리스트 추가/제거 기능 추가 (#27)

--- assets/js/new-common.js
+++ assets/js/new-common.js
@@ -1,5 +1,9 @@
-var inputClear = function() {
-    $('.input-clear').each(function() {
+String.prototype.replaceAt = function(index, character) {
+    return this.substr(0, index) + character;
+};
+
+var setInputClear = function(selector) {
+    $('.' + selector).each(function() {
         var $clearTarget = $(this).prev('input'),
             $clearBtn = $(this);
 
@@ -24,12 +28,72 @@ var setDatePicker = function() {
     $('.input-date').datepicker();
 };
 
+var setPersonalInfoList = function() {
+    $('.js-list').each(function() {
+        var count = $(this).closest('.inner').find('.info-list').length;
+        var $addBtn = $(this).find('.add'),
+            $removeBtn = $(this).find('.remove');
+            
+        $addBtn.on('click', function() {
+            var cloneList = $(this).closest('.inner').find('.info-list').eq(0).clone(true, false);
+
+            count++;
+
+            cloneList.find('li').find('input').each(function() {
+                var inputId = $(this).attr('id'),
+                    numberIdx = inputId.indexOf(1);
+
+                inputId = inputId.replaceAt(numberIdx, count);
+
+                $(this).val('');
+                $(this).attr('disabled', false);
+                $(this).attr('id', inputId);
+                $(this).closest('li').find('label').attr('for', inputId);
+
+                $(this).on('keyup', function() {
+                    $(this).val() ? $(this).next('.input-clear').addClass('vaild') : $(this).next('.input-clear').removeClass('vaild');
+                });
+                $(this).next('.input-clear').on('click', function() {
+                    $(this).removeClass('vaild');
+                    $(this).prev('input').val("").focus();
+                });
+                
+                if ($(this).hasClass('input-date')) {
+                    $(this).removeClass('hasDatepicker').datepicker();
+                }
+            });
+            $(this).closest('.js-list').before(cloneList);
+        });
+
+        $removeBtn.on('click', function() {
+            var checkdList = [],
+                removeList = [];
+
+            $(this).closest('.inner').find('.check-box').find('input').each(function() {
+                if ($(this).is(':checked')) {
+                    checkdList.push($(this));
+                }
+            });
+
+            for (var i = 0; i < checkdList.length; i++) {
+                removeList.push(checkdList[i].closest('.info-list'));
+            }
+            for (var j = 0; j < removeList.length; j++) {
+                removeList[j].remove();
+            }
+        });
+    });
+};
+
 var init = function() {
     if ($('.input-clear').length) {
-        inputClear();
+        setInputClear('input-clear');
     }
     if ($('.input-date').length) {
         setDatePicker();
+    }
+    if ($('.js-list').length) {
+        setPersonalInfoList();
     }
 };
 
