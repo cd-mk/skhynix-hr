d932f836a7f03ec32abac1c550c206aeb7730564
안홍선 <37172347+cd-hsahn@users.noreply.github.com>
2019-02-25 10:07:05 +0900

    Update: common.js, Delete: new-common.js (#36)

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,191 +1,5 @@
-$(function() {
-
-  var group = $(".personal-info");
-
-  group.each(function() {
-    var _group = new GroupBox(this);
-  });
-
-  // 사용자 정의 생성자 함수 정의
-  function GroupBox(groupElement) {
-
-    var box = $(groupElement).find(".box");
-    var title = $(groupElement).find(".box .title a");
-
-    box.each(function(idx) {
-      var newBox = new RootBox(this);
-      if (idx > 0) {
-        newBox.siblingsClose();
-      }
-    });
-  }
-
-  // 사용자 정의 생성자 함수 정의
-  function RootBox(boxElement) {
-    var _this = this;
-    var boxEl = $(boxElement);
-    var target = $(boxEl).find(".title a");
-    var cont = $(boxEl).find(".cont");
-
-    // _groupParent = $(boxEl).parent();
-
-    target.on("click", anchorClickEvent);
-
-    function anchorClickEvent() {
-
-      if (cont.is(':hidden')) {
-        _this.open();
-      } else {
-        _this.close();
-      }
-    }
-
-    _this.siblingsClose = function() {
-      cont.css('display', 'none');
-    };
-
-    _this.open = function() {
-      cont.slideDown();
-    };
-    _this.close = function() {
-      cont.slideUp();
-    }
-  }
-
-  function searchClear() {
-    $('.search-clear').prev().each(function() {
-      $(this).keyup(function() {
-        $(this).next().toggle(Boolean($(this).val()));
-      });
-      $(this).next().toggle(Boolean($(this).val()));
-      $(this).next().click(function() {
-        $(this).prev().val('').focus();
-        $(this).hide();
-      });
-    });
-  }
-  searchClear();
-
-  function listShow() {
-    $('.js-change-wrap').delegate('.list-change', 'change', function() {
-      var opt = $(this).val();
-
-      $(this).find('option').eq(0).prop('selected', true);
-      $(this).closest('.js-change').find('.info-list').hide();
-      $(this).closest('.js-change').find('.info-list' + '.' + opt).show();
-
-    });
-  }
-  listShow();
-
-  function optionChange() {
-    $('.inner').delegate('.degree select', 'change', function() {
-      var opt = $(this).val();
-
-      if (opt === '중졸' || opt === '고졸') {
-        $(this).closest('.info-list').find('.department-name input').css('opacity', 0);
-        $(this).closest('.info-list').find('.department-name span').css('opacity', 0);
-        $(this).closest('.info-list').find('.grade input').css('opacity', 0);
-        $(this).closest('.info-list').find('.grade span').css('opacity', 0);
-      } else {
-        $(this).closest('.info-list').find('.department-name input').css('opacity', 1);
-        $(this).closest('.info-list').find('.department-name span').css('opacity', 1);
-        $(this).closest('.info-list').find('.grade input').css('opacity', 1);
-        $(this).closest('.info-list').find('.grade span').css('opacity', 1);
-        $(this).closest('.info-list').find('input').attr('disabled', false);
-      }
-    });
-  }
-  optionChange();
-
-  function checkButton() {
-    var isFull;
-
-    $('.inner').delegate('input', 'blur', function() {
-      $(this).closest('.info-list').find('input[type=text]').each(function() {
-        
-        if ($(this).val() === '') {
-          isFull = true;
-        } else {
-          isFull = false;
-        }
-      });
-      if (!isFull) {
-        $(this).closest('.info-list').find('.progress span').css('opacity', 1);
-        $(this).closest('.info-list').find('.certifi a').css('opacity', 1);
-      } else {
-        $(this).closest('.info-list').find('.progress span').css('opacity', 0);
-        $(this).closest('.info-list').find('.certifi a').css('opacity', 0);
-      }
-
-    });
-  }
-  checkButton();
-
-  function setInfoList() {
-    $('.btn-plus').on('click', function() {
-      var addList;
-      var chkWrap = $(this).closest('.inner').hasClass('js-change-wrap') ? true : false;
-
-      if (chkWrap) {
-        addList = $(this).closest('.inner').find('.js-change').eq(0).clone().addClass('cloned');
-
-        if ($(this).closest('.inner').hasClass('info-2')) {
-          addList.find('.progress span').removeAttr('class').addClass('btn wait').text('인증대기').attr('id', 'state-license4');
-          addList.find('.certifi a').removeAttr('class').addClass('btn request').text('인증요청').attr('id', 'btn-license4');
-        }
-      } else {
-        addList = $(this).closest('.inner').find('.info-list').eq(0).clone().addClass('cloned').attr('style', 'display: block');
-
-        if ($(this).closest('.inner').hasClass('info-1')) {
-          addList.find('.progress span').removeAttr('class').addClass('btn wait').text('인증대기').attr('id', 'state-impossible2');
-          addList.find('.certifi a').removeAttr('class').addClass('btn request').text('인증요청').attr('id', 'btn-impossible2');
-        } else if ($(this).closest('.inner').hasClass('info-3')) {
-          addList.find('.progress span').removeAttr('class').addClass('btn wait').text('인증대기').attr('id', 'state-career4');
-          addList.find('.certifi a').removeAttr('class').addClass('btn request').text('인증요청').attr('id', 'btn-career4');
-        }
-      }
-      addList.find('input').val('');
-      addList.find('input').attr('disabled', false);
-      addList.find('.degree select').find('option').eq(2).prop('selected', true);
-      addList.find('.progress span').css('opacity', 0);
-      addList.find('.certifi a').css('opacity', 0);
-      $(this).closest('.add-list').before(addList);
-
-      popup6();
-      license4();
-      career4();
-    });
-
-    $('.btn-minus').on('click', function() {
-      if ($(this).closest('.inner').find('.info-list').length > 1) {
-        $(this).closest('.add-list').prev('.cloned').remove();
-      }
-    });
-  }
-  setInfoList();
-});
-
-
-/**
- * *************
- * new common js
- * *************
- */
-
-var inputClear = function() {
-  $('.input-clear').each(function() {
-      var $clearTarget = $(this).prev('input'),
-          $clearBtn = $(this);
-
-      $clearTarget.on('keyup', function() {
-        $(this).val() ? $clearBtn.addClass('vaild') : $clearBtn.removeClass('vaild');
-      });
-      $clearBtn.on('click', function() {
-        $(this).removeClass('vaild');
-        $(this).prev('input').val("").focus();
-      });
-  });
+String.prototype.replaceAt = function(index, character) {
+  return this.substr(0, index) + character;
 };
 
 var setDatePicker = function() {
@@ -199,13 +13,144 @@ var setDatePicker = function() {
   $('.input-date').datepicker();
 };
 
-var init = function() {
-  if ($('.input-clear').length) {
-    inputClear();
-  }
-  if ($('.input-date').length) {
-    setDatePicker();
+var inputClear = {
+  wrapClass : '.input-box',
+  clearBtnClass : '.input-clear',
+  validateClass : 'vaild',
+  init : function() {
+    this.setKeyUp();
+    this.setClearBtn();
+  },
+  setTarget : function() {
+    var targetInputs = [],
+      targetButtons = [];
+    var clearBtn = this.clearBtnClass;
+
+    $(clearBtn).each(function() {
+      targetButtons.push($(this));
+      targetInputs.push($(this).prev('input'));
+    });
+
+    return [targetInputs, targetButtons];
+  },
+  setKeyUp : function() {
+    var wrapper = $(this.wrapClass).parent();
+    var setTarget = this.setTarget(),
+      targetInputs = setTarget[0];
+
+    for (var i = 0; i < targetInputs.length; i++) {
+      $(wrapper).delegate(targetInputs[i], 'keyup', this.showHideClearBtn(targetInputs[i]));
+    }
+  },
+  setClearBtn : function() {
+    var wrapper = $(this.wrapClass).parent();
+    var setTarget = this.setTarget(),
+      targetButtons = setTarget[1];
+
+    for (var i = 0; i < targetButtons.length; i++) {
+      $(wrapper).delegate(targetButtons[i], 'click', this.clickClearBtn(targetButtons[i]));
+    }
+  },
+  showHideClearBtn : function() {
+    var clearBtnClass = this.clearBtnClass,
+      valiClass = this.validateClass;
+    
+    return function(input) {
+      var $targetInput = $(input.target);
+      var inputValue = $targetInput.val(),
+        $clearBtn = $targetInput.next(clearBtnClass);
+      
+      inputValue ? $clearBtn.addClass(valiClass) : $clearBtn.removeClass(valiClass);
+    };
+  },
+  clickClearBtn : function() {
+    var valiClass = this.validateClass;
+
+    return function(button) {
+      $(button.target).prev('input').val('').focus();
+      $(button.target).removeClass(valiClass);
+    };
   }
 };
 
-$(document).ready(init);
+var setPersonalInfoList = function() {
+  $('.js-list').each(function() {
+      var count = $(this).closest('.inner').find('.info-list').length;
+      var $addBtn = $(this).find('.add'),
+          $removeBtn = $(this).find('.remove');
+
+    $addBtn.on('click', function() {
+      var cloneList = $(this).closest('.inner').find('.info-list').eq(0).clone(true, false);
+
+      count++;
+
+      cloneList.find('li').find('input').each(function() {
+        var inputId = $(this).attr('id'),
+            numberIdx = inputId.indexOf(1);
+
+        inputId = inputId.replaceAt(numberIdx, count);
+
+        $(this).val('');
+        $(this).attr('disabled', false);
+        $(this).attr('id', inputId);
+        $(this).closest('li').find('label').attr('for', inputId);
+
+        if ($(this).hasClass('input-date')) {
+          $(this).removeClass('hasDatepicker').datepicker();
+        }
+      });
+      $(this).closest('.js-list').before(cloneList);
+    });
+
+    $removeBtn.on('click', function() {
+        var checkdList = [],
+            removeList = [];
+
+        $(this).closest('.inner').find('.check-box').find('input').each(function() {
+          if ($(this).is(':checked')) {
+            checkdList.push($(this));
+          }
+        });
+
+        for (var i = 0; i < checkdList.length; i++) {
+          removeList.push(checkdList[i].closest('.info-list'));
+        }
+        for (var j = 0; j < removeList.length; j++) {
+          removeList[j].remove();
+        }
+    });
+  });
+};
+
+var layerPopup = function() {
+  $('.js-popup-open').each(function() {
+    $(this).on('click', function() {
+      $(this).next('.layer-popup').addClass('open');
+
+      $('body').addClass('layer-open');
+    });
+  });
+
+  $('.js-popup-close').each(function() {
+    $(this).on('click', function() {
+      $(this).closest('.layer-popup').removeClass('open');
+
+      $('body').removeClass('layer-open');
+    });
+  });
+};
+
+$(document).ready(function() {
+  if ($('.input-date').length) {
+    setDatePicker();
+  }
+  if ($('.input-clear').length) {
+    inputClear.init();
+  }
+  if ($('.js-list').length) {
+    setPersonalInfoList();
+  }
+  if ($('.layer-popup').length) {
+    layerPopup();
+  }
+});
